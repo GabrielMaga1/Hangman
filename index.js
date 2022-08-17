8fbbd34f19fe5fbbc04c3baffdb6d15def35f452
GabrielMaga1 <109106108+GabrielMaga1@users.noreply.github.com>
2022-08-17 15:31:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var miss = 0; 
 var cuv;
 var length2 = 0;
-var length1;
+var length1 = 0;
 const lit = []; 
 var divElem = document.createElement('div');
 divElem.setAttribute('style', 'text-align:center;');
@@ -19,29 +19,33 @@ function generate(){
     document.getElementById("word").value = "";
     document.body.appendChild(divElem);
     document.getElementById("word").disabled = true;
+    document.getElementById("joaca").disabled = true;
   
 }
+
 function search(char){
-    let cont = 0;
-    for(let i = 0; i < length1; ++i){
-        if(cuv[i] == char || cuv[i] == char.toLowerCase()){
+    if(length1 != 0){
+        let cont = 0;
+        for(let i = 0; i < length1; ++i){
+            if(cuv[i] == char || cuv[i] == char.toLowerCase()){
             lit[i].innerHTML = char;
             ++cont;
             ++length2;
+            }
+     }
+       if(cont == 0){
+            ++miss;
+            document.getElementById(miss).style.display ="inline";
+            if(miss == 6){
+                window.setTimeout(function(){alert("You lost")},500);
+                window.setTimeout(function(){location.reload()},1500);
+            }
         }
-    }
-    if(cont == 0){
-        ++miss;
-        document.getElementById(miss).style.display ="inline";
-        if(miss == 6){
-            window.setTimeout(function(){alert("You lost")},500);
+        if(length2 == length1){
+            window.setTimeout(function(){alert("Yow Win!")},900);
             window.setTimeout(function(){location.reload()},1500);
-        }
-    }
-    if(length2 == length1){
-        window.setTimeout(function(){alert("Yow Win!")},900);
-        window.setTimeout(function(){location.reload()},1500);
         
+        }
+        document.getElementById(char).disabled = true;
     }
-    document.getElementById(char).disabled = true;
 }
