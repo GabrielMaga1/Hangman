55ded73b88cabf9dbfca4b961c332bed314eb058
GabrielMaga1 <109106108+GabrielMaga1@users.noreply.github.com>
2022-08-21 18:15:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 var miss = 0; 
-var cuv;
-var length2 = 0;
-var length1 = 0;
-const lit = []; 
+var word;
+var length = 0;
+const character = []; 
 var divElem = document.createElement('div');
 divElem.setAttribute('style', 'text-align:center;');
 function generate(){
-    cuv = document.getElementById("word").value;
-    length1 = cuv.length;
-    for(let i = 0; i < length1; ++i){
-        lit[i] = document.createElement('button');
-        lit[i].innerHTML = "_";
-        lit[i].setAttribute('number', i);
-        lit[i].setAttribute('class', "FindWord");
-        document.body.appendChild(lit[i]);
-        divElem.appendChild(lit[i]);
+    word = document.getElementById("word").value;
+    for(let i = 0; i < word.length; ++i){
+        character[i] = document.createElement('button');
+        character[i].innerHTML = "_";
+        character[i].setAttribute('number', i);
+        character[i].setAttribute('class', "FindWord");
+        document.body.appendChild(character[i]);
+        divElem.appendChild(character[i]);
     }
     document.getElementById("word").value = "";
     document.body.appendChild(divElem);
@@ -24,13 +22,13 @@ function generate(){
 }
 
 function search(char){
-    if(length1 != 0){
+    if(word.length != 0){
         let cont = 0;
-        for(let i = 0; i < length1; ++i){
-            if(cuv[i] == char || cuv[i] == char.toLowerCase()){
-            lit[i].innerHTML = char;
+        for(let i = 0; i < word.length; ++i){
+            if(word[i] == char || word[i] == char.toLowerCase()){
+            character[i].innerHTML = char;
             ++cont;
-            ++length2;
+            ++length;
             }
      }
        if(cont == 0){
@@ -41,7 +39,7 @@ function search(char){
                 window.setTimeout(function(){location.reload()},1500);
             }
         }
-        if(length2 == length1){
+        if(length == word.length){
             window.setTimeout(function(){alert("Yow Win!")},900);
             window.setTimeout(function(){location.reload()},1500);
         
